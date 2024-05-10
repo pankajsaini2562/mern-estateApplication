daec513af2495402047a6f49d880b16e299e9828
pankajsaini <pankajsaini2762@gmail.com>
2024-05-10 14:23:20 +0530

    create message and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Signin from "../pages/Signin";
+import Signout from "../pages/Signout";
+import Profile from "../pages/Profile";
+import About from "../pages/About";
+import Signout from "../pages/Signout";
 export default function App() {
-  return <div className="font-bold text-fuchsia-700">dfhrtyrj</div>;
+  return;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signout" element={<Signout />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>;
 }
